5e3b4d51c74a86cfd2f7e0fcb11dd879775169f0
Ali nadhum <alinadhum1998@gmail.com>
2024-09-21 17:59:07 +0300

    add create category

---
+++ controllers/CategoryController.js
@@ -0,0 +1,43 @@
+const asyncHandler = require("express-async-handler");
+const {
+  CategoryModel,
+  ValidateCreateCategory,
+  ValidateUpdateCategory,
+} = require("../models/CategoryModel");
+
+// ==================================
+// @desc Create a new category
+// @route /api/category
+// @method POST
+// @access private (only admin)
+// ==================================
+module.exports.createCategory = asyncHandler(async (req, res) => {
+  try {
+    // validtion input data
+    const { error } = ValidateCreateCategory(req.body);
+    if (error) {
+      return res.status(400).json({ message: error.details[0].message });
+    }
+
+    // Check if category title is unique
+    const existingCategory = await CategoryModel.findOne({
+      title: req.body.title,
+    });
+
+    if (existingCategory) {
+      return res
+        .status(400)
+        .json({ message: "القسم موجود بالفعل يرجى اختيار اسم اخر " });
+    }
+
+    // create and save category in database
+    const category = await CategoryModel.create({
+        title: req.body.title
+    })
+
+    res.status(201).json({message: "تم اضافه القسم بنجاح"})
+
+  } catch (error) {
+    res.status(500).json({ message: "فشل في اضافه القسم" });
+  }
+});
